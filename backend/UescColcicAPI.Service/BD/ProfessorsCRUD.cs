972c1f54bdf800857b4a51d4d3079a852272ff83
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-09-21 20:59:16 -0300

    Add ProfessorsCRUD class for CRUD operations on Professor entities

---
+++ backend/UescColcicAPI.Service/BD/ProfessorsCRUD.cs
@@ -0,0 +1,33 @@
+using UescColcicAPI.Services.BD.Interfaces;
+using UescColcicAPI.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UescColcicAPI.Services.BD;
+public class ProfessorsCRUD : IProfessorsCRUD
+{
+    public void Create(Professor entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(Professor entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Professor> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Professor? ReadById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Professor entity)
+    {
+        throw new NotImplementedException();
+    }
+}
