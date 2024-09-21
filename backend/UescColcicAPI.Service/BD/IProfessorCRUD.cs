fb0427ac2c4de085bed89d2de3eea08a9da3b186
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-09-21 20:59:03 -0300

    Refactor IProfessorCRUD to use interface name IProfessorsCRUD

--- backend/UescColcicAPI.Service/BD/IProfessorCRUD.cs
+++ backend/UescColcicAPI.Service/BD/IProfessorCRUD.cs
@@ -3,31 +3,6 @@ using UescColcicAPI.Core;
 
 namespace UescColcicAPI.Services.BD.Interfaces;
 
-public class IProfessorCRUD : IBaseCRUD<Professor>
+public interface IProfessorsCRUD : IBaseCRUD<Professor>
 {
-    public void Create(Professor entity)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Delete(Professor entity)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Professor? ReadById(int id)
-    {
-        throw new NotImplementedException();
-    }
-
-    public IEnumerable<Professor> ReadAll()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Update(Professor entity)
-    {
-        throw new NotImplementedException();
-    }
-
 }
