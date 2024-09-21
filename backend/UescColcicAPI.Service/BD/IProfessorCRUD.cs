21550c3b8335fac447e644acaed9a68b099cb5f6
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-09-21 20:15:00 -0300

    Add IProfessorCRUD interface for CRUD operations on Professor entities

---
+++ backend/UescColcicAPI.Service/BD/IProfessorCRUD.cs
@@ -0,0 +1,33 @@
+using System;
+using UescColcicAPI.Core;
+
+namespace UescColcicAPI.Services.BD.Interfaces;
+
+public class IProfessorCRUD : IBaseCRUD<Professor>
+{
+    public void Create(Professor entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(Professor entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Professor? ReadById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Professor> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Professor entity)
+    {
+        throw new NotImplementedException();
+    }
+
+}
