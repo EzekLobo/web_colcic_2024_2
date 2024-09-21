86434664d3b5e19531679416d734ca13993a4e88
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-09-21 14:18:45 -0300

    feat: Add StudentViewModel for creating and updating students

---
+++ backend/UescColcicAPI.Service/ViewModel/StudantViewModel.cs
@@ -0,0 +1,8 @@
+namespace UescColcicAPI.Services.ViewModels
+{
+    public class StudentViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
