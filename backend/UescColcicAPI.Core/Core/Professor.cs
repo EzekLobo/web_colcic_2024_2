c1e6c524b68b1801bcb02dceaa9e66fb249744e1
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-09-21 20:14:31 -0300

    Add Professor class

---
+++ backend/UescColcicAPI.Core/Core/Professor.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace UescColcicAPI.Core;
+
+public class Professor
+{
+    public int ProfessorId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Departament { get; set; }
+    public string Bio { get; set; }
+
+}
