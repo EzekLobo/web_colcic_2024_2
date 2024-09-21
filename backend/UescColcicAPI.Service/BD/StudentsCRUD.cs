08565aa1b3a83f34a96b037c74bf2ba896dee362
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-09-21 14:19:32 -0300

    Refactor StudentsCRUD to use StudentViewModel for creating and updating students

--- backend/UescColcicAPI.Service/BD/StudentsCRUD.cs
+++ backend/UescColcicAPI.Service/BD/StudentsCRUD.cs
@@ -1,54 +1,66 @@
 ﻿using UescColcicAPI.Services.BD.Interfaces;
 using UescColcicAPI.Core;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace UescColcicAPI.Services.BD;
-
-public class StudentsCRUD : IStudentsCRUD
+namespace UescColcicAPI.Services.BD
 {
-    private static readonly List<Student> Students = new()
-   {
-      new Student { StudentId = 1, Name = "Douglas", Email = "douglas.cic@uesc.br" },
-      new Student { StudentId = 2, Name = "Estevão", Email = "estevao.cic@uesc.br" },
-      new Student { StudentId = 3, Name = "Gabriel", Email = "gabriel.cic@uesc.br" },
-      new Student { StudentId = 4, Name = "Gabriela", Email = "gabriela.cic@uesc.br" }
-   };
-    public void Create(Student entity)
+    public class StudentsCRUD : IStudentsCRUD
     {
-        Students.Add(entity);
-    }
+        private static readonly List<Student> Students = new()
+        {
+            new Student { StudentId = 1, Name = "Douglas", Email = "douglas.cic@uesc.br" },
+            new Student { StudentId = 2, Name = "Estevão", Email = "estevao.cic@uesc.br" },
+            new Student { StudentId = 3, Name = "Gabriel", Email = "gabriel.cic@uesc.br" },
+            new Student { StudentId = 4, Name = "Gabriela", Email = "gabriela.cic@uesc.br" }
+        };
 
-    public void Delete(Student entity)
-    {   
-        var student = this.Find(entity.Email);
-        if(student is  not null)
-            Students.Remove(student);
-    }
+        public void Create(Student entity)
+        {
+            // Atribuir um ID único ao novo estudante
+            entity.StudentId = Students.Any() ? Students.Max(s => s.StudentId) + 1 : 1;
+            Students.Add(entity);
+        }
 
-    public IEnumerable<Student> ReadAll()
-    {
-        return Students;
-    }
+        public void Delete(Student entity)
+        {
+            var student = this.Find(entity.StudentId);
+            if (student is not null)
+            {
+                Students.Remove(student);
+            }
+        }
 
-    public Student? ReadById(int id)
-    {
-        var student = this.Find(id);
-        return student;
-    }
+        public IEnumerable<Student> ReadAll()
+        {
+            return Students;
+        }
 
-    public void Update(Student entity)
-    {
-        var student = this.Find(entity.Email);
-        if(student is not null) student.Name = entity.Name;
-    }
+        public Student? ReadById(int id)
+        {
+            return this.Find(id);
+        }
 
-    private Student? Find(string email)
-    {
-        return Students.FirstOrDefault(x => x.Email == email);
-    }
+        public void Update(Student entity)
+        {
+            var student = this.Find(entity.StudentId);
+            if (student is not null)
+            {
+                student.Name = entity.Name;
+                student.Email = entity.Email; 
+            }
+        }
 
-    private Student? Find(int id)
-    {
-        return Students.FirstOrDefault(x => x.StudentId == id);
-    }
+        // Busca um estudante por email
+        private Student? Find(string email)
+        {
+            return Students.FirstOrDefault(x => x.Email == email);
+        }
 
+        // Busca um estudante por ID
+        private Student? Find(int id)
+        {
+            return Students.FirstOrDefault(x => x.StudentId == id);
+        }
+    }
 }
