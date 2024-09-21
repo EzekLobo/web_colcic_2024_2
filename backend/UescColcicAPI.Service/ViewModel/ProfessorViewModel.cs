bae8c61af8d29f54fc4b67bae09f225a96d81852
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-09-21 20:15:14 -0300

    Add ProfessorViewModel class for managing professor data

---
+++ backend/UescColcicAPI.Service/ViewModel/ProfessorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace UescColcicAPI.Services.ViewModel;
+
+public class ProfessorViewModel
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Departament { get; set; }
+    public string Bio { get; set; }
+
+}
