4e8b3ecaa470b2a585ac198090246fe64bc3ae3a
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-09-21 21:54:19 -0300

    Refactor property  in Professor and ProfessorViewModel classes

--- backend/UescColcicAPI.Core/Core/Professor.cs
+++ backend/UescColcicAPI.Core/Core/Professor.cs
@@ -7,7 +7,7 @@ public class Professor
     public int ProfessorId { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
-    public string Departament { get; set; }
+    public string Department { get; set; }
     public string Bio { get; set; }
 
 }
--- backend/UescColcicAPI.Service/ViewModel/ProfessorViewModel.cs
+++ backend/UescColcicAPI.Service/ViewModel/ProfessorViewModel.cs
@@ -1,12 +1,10 @@
-using System;
-
 namespace UescColcicAPI.Services.ViewModel;
 
 public class ProfessorViewModel
 {
     public string Name { get; set; }
     public string Email { get; set; }
-    public string Departament { get; set; }
+    public string Department { get; set; }
     public string Bio { get; set; }
 
 }
